291cd952044f2782f96e56dc067478efc50b522c
Jthornburg1 <reliable2192@gmail.com>
2018-01-14 16:39:15 -0500

    adds buttons and image and title

--- LegoShare/ViewController.swift
+++ LegoShare/ViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var logoContainerView: UIView!
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+    @IBOutlet weak var appNameLabel: UILabel!
+    
+    @IBOutlet weak var buttonContainerView: UIView!
+    
+    @IBOutlet weak var libraryButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
