efd504c37c0d710704884efb8fe228c15c66214c
Jthornburg1 <reliable2192@gmail.com>
2018-01-14 22:21:28 -0500

    wip

--- LegoShare/ViewController.swift
+++ LegoShare/ViewController.swift
@@ -11,17 +11,26 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var logoContainerView: UIView!
-    
     @IBOutlet weak var logoImageView: UIImageView!
-    
     @IBOutlet weak var appNameLabel: UILabel!
-    
     @IBOutlet weak var buttonContainerView: UIView!
-    
     @IBOutlet weak var libraryButton: UIButton!
+    @IBOutlet weak var createProjectButton: UIButton!
+    @IBOutlet weak var myProjectsButton: UIButton!
+    @IBOutlet weak var settingsButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // IBActions
+    @IBAction func didTapSettings(_ sender: Any) {
+    }
+    @IBAction func didTapLibrary(_ sender: Any) {
+    }
+    @IBAction func didTapMyProjects(_ sender: Any) {
+    }
+    @IBAction func didTapCreateProject(_ sender: Any) {
     }
 }
 
